026cf342c867ddc59313c3f4a67f3947d2880573
tzar-beep <vaibhavkushwaha66@gmail.com>
2026-04-16 23:08:09 +0530

    UC4: Maintain Ordered Bogie IDs using LinkedList

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.LinkedList;
 
 public class TrainConsistManagementApp {
     public static void main(String[] args) {
@@ -12,29 +13,36 @@ public class TrainConsistManagementApp {
         System.out.println("Initial bogie count: " + consist.size());
 
         List<String> passengerBogies = new ArrayList<>();
-
         passengerBogies.add("Sleeper");
         passengerBogies.add("AC Chair");
         passengerBogies.add("First Class");
-
         System.out.println("Passenger bogies after addition: " + passengerBogies);
 
         passengerBogies.remove("AC Chair");
-
         System.out.println("After removing AC Chair: " + passengerBogies);
-
         System.out.println("Is Sleeper present? " + passengerBogies.contains("Sleeper"));
-
         System.out.println("Final passenger bogies: " + passengerBogies);
 
         Set<String> bogieIds = new HashSet<>();
-
         bogieIds.add("B101");
         bogieIds.add("B102");
         bogieIds.add("B103");
         bogieIds.add("B101");
-        bogieIds.add("B102");
-
         System.out.println("Unique Bogie IDs: " + bogieIds);
+
+        LinkedList<String> train = new LinkedList<>();
+
+        train.add("Engine");
+        train.add("Sleeper");
+        train.add("AC");
+        train.add("Cargo");
+        train.add("Guard");
+
+        train.add(2, "Pantry Car");
+
+        train.removeFirst();
+        train.removeLast();
+
+        System.out.println("Final Train Consist: " + train);
     }
 }
