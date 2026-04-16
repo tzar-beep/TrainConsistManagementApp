e542c9e7623a4bfc285a836d468803027230b90c
tzar-beep <vaibhavkushwaha66@gmail.com>
2026-04-16 23:43:21 +0530

    UC19: Binary Search for Bogie ID

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,29 @@
+import java.util.Arrays;
+
 public class TrainConsistManagementApp {
     public static void main(String[] args) {
 
-        String[] bogieIds = {"BG101", "BG205", "BG309", "BG412", "BG550"};
+        String[] bogieIds = {"BG309", "BG101", "BG550", "BG205", "BG412"};
         String searchKey = "BG309";
 
+        Arrays.sort(bogieIds);
+
+        int low = 0;
+        int high = bogieIds.length - 1;
         boolean found = false;
 
-        for (int i = 0; i < bogieIds.length; i++) {
-            if (bogieIds[i].equals(searchKey)) {
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            int result = bogieIds[mid].compareTo(searchKey);
+
+            if (result == 0) {
                 found = true;
                 break;
+            } else if (result < 0) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
 
